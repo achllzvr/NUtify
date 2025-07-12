de76bf7d5cb376d9268d5d2877218ca1e41bddbb
Achilles Vonn Rabina <140421417+achllzvr@users.noreply.github.com>
2025-07-12 15:37:40 +0800

    Refactor upcoming appointments section to handle empty state and improve layout

--- nutify/lib/pages/studentHome.dart
+++ nutify/lib/pages/studentHome.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:nutify/models/recentProfessorsModel.dart';
 import 'package:nutify/models/studentHomeAppointments.dart';
@@ -19,141 +17,158 @@ class StudentHome extends StatelessWidget {
         children: [
           studentSearchBar(),
           mostRecentProfessors(recentProfessors),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  'Your Upcoming Appointments...',
-                  style: TextStyle(
-                    fontFamily: 'Arimo',
-                    fontSize: 12,
-                    color: Colors.black87,
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                height: 260,
-                child: ListView.separated(
-                  scrollDirection: Axis.vertical,
-                  separatorBuilder: (context, index) => SizedBox(height: 10),
-                  padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
-                  itemCount: StudentHomeAppointments.getStudentHomeAppointments().length,
-                  itemBuilder: (context, index) {
-                    var appointment = StudentHomeAppointments.getStudentHomeAppointments()[index];
-                    return GestureDetector(
-                      onTap: () {
-                        // Handle appointment tap
-                        print('Tapped on appointment with ID: ${appointment.id}');
-                      },
-                        child: Container(
-                        width: 150,
-                        height: 160,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                          colors: [
-                            const Color(0xFFFFFFFF),
-                            const Color(0xFFE8E8E8),
-                          ],
-                          begin: const FractionalOffset(0.0, 0.0),
-                          end: const FractionalOffset(0.0, 1.0),
-                          stops: [0.0, 1.0],
-                          tileMode: TileMode.clamp,
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(
-                          color: Colors.black.withOpacity(0.095),
-                          width: 1,
-                          ),
-                          boxShadow: [
-                          BoxShadow(
-                            color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.083),
-                            spreadRadius: 2,
-                            blurRadius: 7,
-                            offset: Offset(0, 3),
-                          ),
-                          ],
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                          Text(
-                            appointment.name,
-                            style: TextStyle(
-                            fontFamily: 'Arimo',
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                            ),
-                          ),
-                          Text(
-                            appointment.timestamp,
-                            style: TextStyle(
-                            fontFamily: 'Arimo',
-                            fontSize: 14,
-                            color: Colors.grey.shade600,
-                            ),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  const Color.fromARGB(255, 237, 194, 3),
-                                  const Color.fromARGB(255, 242, 213, 86),
-                                ],
-                                begin: const FractionalOffset(0.0, 0.0),
-                                end: const FractionalOffset(1.0, 1.0),
-                                stops: [0.0, 1.0],
-                                tileMode: TileMode.clamp,
-                              ),
-                              borderRadius: BorderRadius.circular(8),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.02),
-                                  spreadRadius: 4,
-                                  blurRadius: 5,
-                                  offset: Offset(0, 0),
-                                ),
-                              ],
-                            ),
-                            child: ElevatedButton(
-                              onPressed: () {
-                              // Handle view details button press
-                              print('View Details for appointment ID: ${appointment.id}');
-                              },
-                              style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.transparent,
-                              foregroundColor: Colors.white,
-                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                              textStyle: const TextStyle(
-                                fontFamily: 'Arimo',
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              elevation: 0,
-                              ),
-                              child: const Text('View Details'),
-                            ),
-                          ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              // Here you can add the widget to display appointments
-            ],
-          )
+          upcomingAppointments()
         ],
       )
     );
+  }
+
+  Column upcomingAppointments() {
+    List<dynamic> appointments = StudentHomeAppointments.getStudentHomeAppointments();
+    
+    return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text(
+                'Your Upcoming Appointments...',
+                style: TextStyle(
+                  fontFamily: 'Arimo',
+                  fontSize: 12,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: 260,
+              child: appointments.isEmpty
+                ? Center(
+                    child: Text(
+                      'No upcoming appointments',
+                      style: TextStyle(
+                        fontFamily: 'Arimo',
+                        fontSize: 16,
+                        color: Colors.grey.shade600,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  )
+                : ListView.separated(
+                    scrollDirection: Axis.vertical,
+                    separatorBuilder: (context, index) => SizedBox(height: 10),
+                    padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+                    itemCount: appointments.length,
+                    itemBuilder: (context, index) {
+                      var appointment = appointments[index];
+                      return GestureDetector(
+                        onTap: () {
+                          // Handle appointment tap
+                          print('Tapped on appointment with ID: ${appointment.id}');
+                        },
+                          child: Container(
+                          width: 150,
+                          height: 160,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                            colors: [
+                              const Color(0xFFFFFFFF),
+                              const Color(0xFFE8E8E8),
+                            ],
+                            begin: const FractionalOffset(0.0, 0.0),
+                            end: const FractionalOffset(0.0, 1.0),
+                            stops: [0.0, 1.0],
+                            tileMode: TileMode.clamp,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                            color: Colors.black.withOpacity(0.095),
+                            width: 1,
+                            ),
+                            boxShadow: [
+                            BoxShadow(
+                              color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.083),
+                              spreadRadius: 2,
+                              blurRadius: 7,
+                              offset: Offset(0, 3),
+                            ),
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                            Text(
+                              appointment.name,
+                              style: TextStyle(
+                              fontFamily: 'Arimo',
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              appointment.timestamp,
+                              style: TextStyle(
+                              fontFamily: 'Arimo',
+                              fontSize: 14,
+                              color: Colors.grey.shade600,
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    const Color.fromARGB(255, 237, 194, 3),
+                                    const Color.fromARGB(255, 242, 213, 86),
+                                  ],
+                                  begin: const FractionalOffset(0.0, 0.0),
+                                  end: const FractionalOffset(1.0, 1.0),
+                                  stops: [0.0, 1.0],
+                                  tileMode: TileMode.clamp,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.02),
+                                    spreadRadius: 4,
+                                    blurRadius: 5,
+                                    offset: Offset(0, 0),
+                                  ),
+                                ],
+                              ),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                // Handle view details button press
+                                print('View Details for appointment ID: ${appointment.id}');
+                                },
+                                style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.transparent,
+                                foregroundColor: Colors.white,
+                                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                textStyle: const TextStyle(
+                                  fontFamily: 'Arimo',
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                elevation: 0,
+                                ),
+                                child: const Text('View Details'),
+                              ),
+                            ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+            ),
+          ],
+        );
   }
 
   Column mostRecentProfessors(List<RecentProfessor> recentProfessors) {
@@ -176,59 +191,71 @@ class StudentHome extends StatelessWidget {
             ),
             Container(
               height: 150,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                separatorBuilder: (context, index) => SizedBox(width: 15),
-                padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
-                itemCount: recentProfessors.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      // Access the professor ID here
-                      String professorId = recentProfessors[index].id;
-                      String professorName = recentProfessors[index].name;
-                      
-                      print('Tapped on professor: $professorName (ID: $professorId)');
-                      
-                    },
-                    child: Container(
-                      width: 150,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: index % 2 == 0
-                            ? [Color(0xFF1A2049), Color(0xFF35408E)]
-                            : [Color(0xFF35408E), Color(0xFF1A2049)],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.2),
-                            spreadRadius: 2,
-                            blurRadius: 4,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            recentProfessors[index].name,
-                            style: TextStyle(
-                              fontFamily: 'Arimo',
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
+              child: recentProfessors.isEmpty 
+                ? Center(
+                    child: Text(
+                      'No recent professors',
+                      style: TextStyle(
+                        fontFamily: 'Arimo',
+                        fontSize: 16,
+                        color: Colors.grey.shade600,
+                        fontStyle: FontStyle.italic,
                       ),
                     ),
-                  );
-                },
-              ),
+                  )
+                : ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    separatorBuilder: (context, index) => SizedBox(width: 15),
+                    padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+                    itemCount: recentProfessors.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          // Access the professor ID here
+                          String professorId = recentProfessors[index].id;
+                          String professorName = recentProfessors[index].name;
+                          
+                          print('Tapped on professor: $professorName (ID: $professorId)');
+                          
+                        },
+                        child: Container(
+                          width: 150,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: index % 2 == 0
+                                ? [Color(0xFF1A2049), Color(0xFF35408E)]
+                                : [Color(0xFF35408E), Color(0xFF1A2049)],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.2),
+                                spreadRadius: 2,
+                                blurRadius: 4,
+                                offset: Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                recentProfessors[index].name,
+                                style: TextStyle(
+                                  fontFamily: 'Arimo',
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
             )
           ],
         );
