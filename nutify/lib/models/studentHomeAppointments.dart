57b99478a97b3474d23b2bd06382d25d6c2e38c4
Achilles Vonn Rabina <140421417+achllzvr@users.noreply.github.com>
2025-07-12 15:28:01 +0800

    Add StudentHomeAppointments model and integrate upcoming appointments section in StudentHome page

---
+++ nutify/lib/models/studentHomeAppointments.dart
@@ -0,0 +1,24 @@
+class StudentHomeAppointments {
+  final String name;
+  final String id;
+  final String timestamp;
+  final bool isSelected;
+
+  StudentHomeAppointments({
+    required this.name,
+    required this.id,
+    required this.timestamp,
+    this.isSelected = false,
+  });
+
+  static List<StudentHomeAppointments> getStudentHomeAppointments() {
+    List<StudentHomeAppointments> studentHomeAppointments = [];
+
+    studentHomeAppointments.add(StudentHomeAppointments(name: 'Dr. Smith', id: '001', timestamp: '2023-10-01 10:00', isSelected: false));
+    studentHomeAppointments.add(StudentHomeAppointments(name: 'Prof. Johnson', id: '002', timestamp: '2023-10-02 11:00', isSelected: false));
+    studentHomeAppointments.add(StudentHomeAppointments(name: 'Dr. Brown', id: '003', timestamp: '2023-10-03 12:00', isSelected: false));
+
+    return studentHomeAppointments;
+  }
+
+}
--- nutify/lib/pages/studentHome.dart
+++ nutify/lib/pages/studentHome.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:nutify/models/recentProfessorsModel.dart';
+import 'package:nutify/models/studentHomeAppointments.dart';
 
 class StudentHome extends StatelessWidget {
   StudentHome({super.key});
@@ -13,10 +16,141 @@ class StudentHome extends StatelessWidget {
       backgroundColor: const Color(0xFFF5F5F5),
       appBar: studentAppBar(),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           studentSearchBar(),
-          mostRecentProfessors(recentProfessors)
+          mostRecentProfessors(recentProfessors),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text(
+                  'Your Upcoming Appointments...',
+                  style: TextStyle(
+                    fontFamily: 'Arimo',
+                    fontSize: 12,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 260,
+                child: ListView.separated(
+                  scrollDirection: Axis.vertical,
+                  separatorBuilder: (context, index) => SizedBox(height: 10),
+                  padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+                  itemCount: StudentHomeAppointments.getStudentHomeAppointments().length,
+                  itemBuilder: (context, index) {
+                    var appointment = StudentHomeAppointments.getStudentHomeAppointments()[index];
+                    return GestureDetector(
+                      onTap: () {
+                        // Handle appointment tap
+                        print('Tapped on appointment with ID: ${appointment.id}');
+                      },
+                        child: Container(
+                        width: 150,
+                        height: 160,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                          colors: [
+                            const Color(0xFFFFFFFF),
+                            const Color(0xFFE8E8E8),
+                          ],
+                          begin: const FractionalOffset(0.0, 0.0),
+                          end: const FractionalOffset(0.0, 1.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                          color: Colors.black.withOpacity(0.095),
+                          width: 1,
+                          ),
+                          boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.083),
+                            spreadRadius: 2,
+                            blurRadius: 7,
+                            offset: Offset(0, 3),
+                          ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                          Text(
+                            appointment.name,
+                            style: TextStyle(
+                            fontFamily: 'Arimo',
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            appointment.timestamp,
+                            style: TextStyle(
+                            fontFamily: 'Arimo',
+                            fontSize: 14,
+                            color: Colors.grey.shade600,
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  const Color.fromARGB(255, 237, 194, 3),
+                                  const Color.fromARGB(255, 242, 213, 86),
+                                ],
+                                begin: const FractionalOffset(0.0, 0.0),
+                                end: const FractionalOffset(1.0, 1.0),
+                                stops: [0.0, 1.0],
+                                tileMode: TileMode.clamp,
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.02),
+                                  spreadRadius: 4,
+                                  blurRadius: 5,
+                                  offset: Offset(0, 0),
+                                ),
+                              ],
+                            ),
+                            child: ElevatedButton(
+                              onPressed: () {
+                              // Handle view details button press
+                              print('View Details for appointment ID: ${appointment.id}');
+                              },
+                              style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              foregroundColor: Colors.white,
+                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                              textStyle: const TextStyle(
+                                fontFamily: 'Arimo',
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              elevation: 0,
+                              ),
+                              child: const Text('View Details'),
+                            ),
+                          ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              // Here you can add the widget to display appointments
+            ],
+          )
         ],
       )
     );
@@ -24,6 +158,7 @@ class StudentHome extends StatelessWidget {
 
   Column mostRecentProfessors(List<RecentProfessor> recentProfessors) {
     return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.only(left: 20.0),
@@ -59,13 +194,19 @@ class StudentHome extends StatelessWidget {
                     child: Container(
                       width: 150,
                       decoration: BoxDecoration(
-                        color: index % 2 == 0 ? Color.fromARGB(228, 26, 32, 73) : Color.fromARGB(226, 53, 63, 142),
+                        gradient: LinearGradient(
+                          colors: index % 2 == 0
+                            ? [Color(0xFF1A2049), Color(0xFF35408E)]
+                            : [Color(0xFF35408E), Color(0xFF1A2049)],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
                         borderRadius: BorderRadius.circular(10),
                         boxShadow: [
                           BoxShadow(
-                            color: Colors.grey.withOpacity(0.1),
+                            color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.2),
                             spreadRadius: 2,
-                            blurRadius: 5,
+                            blurRadius: 4,
                             offset: Offset(0, 3), // changes position of shadow
                           ),
                         ],
@@ -134,17 +275,18 @@ class StudentHome extends StatelessWidget {
 
   AppBar studentAppBar() {
     return AppBar(
-      title: const Text(
-        'Student Home',
-
-        style: TextStyle(
-          fontFamily: 'Arimo',
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
+      title: Container(
+        margin: const EdgeInsets.only(left: 10.0),
+        child: const Text(
+          'Student Home',
+          style: TextStyle(
+            fontFamily: 'Arimo',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
       ),
-
       centerTitle: false,
       elevation: 0,
       flexibleSpace: Container(
@@ -168,7 +310,7 @@ class StudentHome extends StatelessWidget {
             // Handle profile icon tap
           },
           child: Container(
-            margin: const EdgeInsets.all(10.0),
+            margin: const EdgeInsets.only(right: 25.0),
             child: CircleAvatar(
               backgroundImage: AssetImage('assets/icons/student.png'),
             ),
